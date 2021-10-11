2522a1fdc1ab0485f280340a749b35f955c55e1f
byjusmx <87218747+byjusmx@users.noreply.github.com>
2021-10-11 08:52:19 -0500

    Update WriteStoryScreen.js

--- screens/WriteStoryScreen.js
+++ screens/WriteStoryScreen.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { StyleSheet, Text, View, TouchableOpacity,KeyboardAvoidingView,ToastAndroid, TextInput} from 'react-native';
 import {Header} from 'react-native-elements';
 import db from '../config'
-//import firebase from 'firebase'
+//importa firebase desde 'firebase'
 
 export default class WriteStoryScreen extends React.Component {
     constructor(props){
@@ -26,7 +26,7 @@ export default class WriteStoryScreen extends React.Component {
             author: '',
             storyText: ''
         })
-        ToastAndroid.show('Your story has been sumitted', ToastAndroid.SHORT)
+        ToastAndroid.show('Tu historia ha sido enviada', ToastAndroid.SHORT)
     }
 
     render(){
@@ -35,12 +35,12 @@ export default class WriteStoryScreen extends React.Component {
                 <Header
                     backgroundColor = {'pink'}
                     centerComponent = {{
-                        text : 'Bed Time Stories',
-                        style : { color: 'white', fontSize: 20}
+                        text : 'Historias para Dormir',
+                        style : { color: 'white', fontSize: 15}
                     }}
                 />
                 <TextInput
-                    placeholder="Story Title"
+                    placeholder="Título de la Historia"
                     onChangeText= {(text)=>{
                         this.setState({
                             title: text
@@ -49,7 +49,7 @@ export default class WriteStoryScreen extends React.Component {
                     value={this.state.title}
                     style={styles.title}/>
                 <TextInput
-                    placeholder="Author"
+                    placeholder="Autor"
                     onChangeText= {(text)=>{
                         this.setState({
                             author: text
@@ -58,7 +58,7 @@ export default class WriteStoryScreen extends React.Component {
                     value={this.state.author}
                     style={styles.author} />
                 <TextInput
-                    placeholder="Write your story"
+                    placeholder="Escribe tu historia"
                     onChangeText= {(text)=>{
                         this.setState({
                             storyText: text
@@ -72,7 +72,7 @@ export default class WriteStoryScreen extends React.Component {
                     style={styles.submitButton}
                     onPress={this.submitStory}
                     >
-                    <Text style={styles.buttonText}>Submit</Text>
+                    <Text style={styles.buttonText}>Enviar</Text>
                 </TouchableOpacity>
             </KeyboardAvoidingView>
         );
